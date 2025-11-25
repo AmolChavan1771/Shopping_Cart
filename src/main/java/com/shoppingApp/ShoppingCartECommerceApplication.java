a900bab8e88d2c3552bd21520a39678c11d9361b
AmolChavan1771 <amolchavan1771@gmail.com>
2025-11-25 13:38:19 +0530

    First Commit

---
+++ src/main/java/com/shoppingApp/ShoppingCartECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartECommerceApplication.class, args);
+	}
+
+}
